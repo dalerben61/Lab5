b59e5d499708c0621cceb2f726b2ac9daa102ba3
unknown <ben.delisle61@gmail.com>
2019-10-06 23:51:51 -0400

    Added the new functions to the library. import the library in the Task3 file

--- Task3.py
+++ Task3.py
@@ -1,4 +1,7 @@
 import time
+import deli0061Library
 
 print("This program executes different GFX Hat functions, and a 5th one that resets the backlight every time a function is done executing.")
-time.sleep(3)
+print("Let's start by drawing a vertical line.")
+time.sleep(5)
+
--- deli0061Library.py
+++ deli0061Library.py
@@ -24,3 +24,71 @@ def calculateDistanceBetweenPoints():
     distance = math.sqrt( ((x - x1) ** 2) + ((y - y1) ** 2) )
     return distance
 
+def verticalLine():
+    lcd.clear()
+    lcd.show()
+
+    x = int(input("Set a value for x. "))
+    y = 0
+    while (y <= 63):
+        lcd.set_pixel(x,y,1)
+        y = y + 1
+
+    backlight.set_all(0,255,0)
+    backlight.show()
+    lcd.show()
+
+def horizontalLine():
+    lcd.clear()
+    lcd.show()
+
+    y = int(input("Set a value for y. "))
+    x = 0
+    while (x <= 127):
+        lcd.set_pixel(x,y,1)
+        x = x + 1
+
+    backlight.set_all(0,255,0)
+    backlight.show()
+    lcd.show()
+
+def staircase():
+    lcd.clear()
+    lcd.show()
+
+    x = 15
+    y = 8
+    w = int(input("Input the width of one stair. "))
+    h = int(input("Set the height of one stair. "))
+
+    while x >= 0 and x <= 127 and y >=0 and y <= 63:
+        for x in range(w):
+            lcd.set_pixel(x,y,1)
+            x = x + 1
+        
+        for y in range(h):
+            lcd.set_pixel
+            y = y + 1
+
+    backlight.set_all(0,255,0)
+    backlight.show()
+    lcd.show()
+
+def randomPixels():
+    lcd.clear()
+    lcd.show()
+    length = float(input("For how much seconds will the program show random pixels on the GFX Hat? "))
+    timestop = time.time() + length
+    backlight.set_all(0,255,0)
+    backlight.show()
+    
+    while time.time() <= timestop:
+        x = random.randint(1,127)
+        y = random.randint(1,63)
+        lcd.set(x,y,1)
+        lcd.show()
+        time.sleep(0.2)
+
+def clearBacklight():
+    backlight.set_all(0,0,0)
+    backlight.show()
